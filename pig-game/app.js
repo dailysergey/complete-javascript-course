7eecb9f2c4e53b419a42e7430ee57eeb20387750
dailysergey <ds@ru>
2019-12-18 17:45:38 +0300

    added new game button

--- pig-game/app.js
+++ pig-game/app.js
@@ -10,22 +10,30 @@ GAME RULES:
 */
 
 var scores, roundScore, activeScore;
+init();
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-dice = Math.floor(Math.random()*6)+1;
-
-
-function updateToNull(){
-
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
     document.querySelector('.dice').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    
 }
-updateToNull();
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
 document.querySelector('.btn-roll').addEventListener('click',function(){//anonymous function
 
     //0.Console the log
