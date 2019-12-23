d04a25947b1f94ab1b6d54cee251b381adcc6e98
daily sergey <guskov13sv@gmail.com>
2019-12-23 14:54:22 +0300

    console.log cleaner

--- 5coursechallenge/app.js
+++ 5coursechallenge/app.js
@@ -13,8 +13,8 @@
 		}
 	}
 	Question.prototype.checkAnswer = function (maybeAnswer) {
-		console.log(this.rightAnswer);
-		console.log(maybeAnswer);
+		console.log('Right answer: ' + this.rightAnswer);
+		console.log('Your answer: ' + maybeAnswer);
 		if (this.rightAnswer === maybeAnswer) {
 			alert('Congrats, you are right!');
 		} else {
@@ -26,14 +26,15 @@
 	var q1 = new Question("What\'s up?", ['Okay', 'Alright', 'Can be better!'], 2);
 	var q2 = new Question('How is on duty today', ['Jonas', 'John', 'Me'], 0);
 	var q3 = new Question('How long does it take?', ['1 minute', '5 minutes', '1 year'], 1);
+	var questions = [q1, q2, q3];
 
 	function nexQuestion() {
-		var questions = [q1, q2, q3];
+
 		var n = Math.floor(Math.random() * questions.length);
-
 		questions[n].displayQuestion();
-
 		var answer = parseInt(prompt(questions[n].question));
 		questions[n].checkAnswer(answer);
+		nexQuestion();
 	}
+	nexQuestion();
 })();
